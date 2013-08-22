ebe84920cbc924b847590bd304c121b9a0bff548
Alon Daks <alondaks@Alons-MacBook-Air.local>
2013-08-22 11:07:22 -0700

    fixed google api key

--- BirdWatching/BirdMasterViewController.m
+++ BirdWatching/BirdMasterViewController.m
@@ -11,7 +11,6 @@
 #import "RushDataController.h"
 #import "Event.h"
 #import "House.h"
-#import <GoogleMaps/GoogleMaps.h>
 
 @interface BirdMasterViewController ()
 
@@ -20,7 +19,6 @@
 @end
 
 @implementation BirdMasterViewController{
-    GMSMapView *mapView_;
 }
 
 - (void)awakeFromNib
@@ -159,24 +157,6 @@
         detailViewController.event = [self.dataController objectInEventsAtIndex:[self.tableView indexPathForSelectedRow].row];
     }
 }
-
-//- (void)loadView {
-//    // Create a GMSCameraPosition that tells the map to display the
-//    // coordinate -33.86,151.20 at zoom level 6.
-//    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-//                                                            longitude:151.20
-//                                                                 zoom:6];
-//    mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
-//    mapView_.myLocationEnabled = YES;
-//    self.view = mapView_;
-//    
-//    // Creates a marker in the center of the map.
-//    GMSMarker *marker = [[GMSMarker alloc] init];
-//    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
-//    marker.title = @"Sydney";
-//    marker.snippet = @"Australia";
-//    marker.map = mapView_;
-//}
 
 
 @end
